8f8c84fd9caaf0ccab9c1f3b37681bdd06b5da9c
Ильнар Ахмадиев <36658509+IlnarAhm@users.noreply.github.com>
2018-11-04 00:14:06 +0300

    Добавил рабочий таймер

---
+++ js/timer.js
@@ -0,0 +1,65 @@
+(function(){
+
+	
+	var _id="0431c4dc9448d4d7714d0cdd064dc696";
+	while(document.getElementById("timer"+_id))_id=_id+"0";
+	document.write("<div id='timer"+_id+"' style='min-width:336px;height:100px;'></div>");
+	var _t=document.createElement("script");
+
+	_t.src="js/timer.min.js";
+
+	var _f=function(_k){
+		var l=new MegaTimer(_id, {
+			"view":[1,1,1,0],
+			"type":{
+				"currentType":"3",
+				"params":{
+					"weekdays":[1,1,1,1,1,1,1],
+					"usertime":true,
+					"time":"00:00",
+					"tz":-180,
+					"hours":"1",
+					"minutes":"1"
+				}
+			}
+			,"design":{
+				"type":"circle",
+				"params":{
+					"width":"3",
+					"radius":"46",
+					"line":"solid",
+					"line-color":"#ffffff",
+					"background":"solid",
+					"background-color":"rgba(255,255,255,0.07)",
+					"direction":"direct",
+					"number-font-family":{
+						"family":"Roboto",
+						"link":"<link href='http://fonts.googleapis.com/css?family=Roboto&subset=latin,cyrillic' rel='stylesheet' type='text/css'>"
+					},
+					"number-font-size":"32",
+					"number-font-color":"#ffffff",
+					"separator-margin":"9",
+					"separator-on":false,
+					"separator-text":":",
+					"text-on":true,
+					"text-font-family":{
+						"family":"Roboto",
+						"link":"<link href='http://fonts.googleapis.com/css?family=Roboto&subset=latin,cyrillic' rel='stylesheet' type='text/css'>"
+					},
+					"text-font-size":"19",
+					"text-font-color":"#ffffff"
+				}
+			},
+			"designId":7,
+			"theme":"black",
+			"width":336,
+			"height":100
+		});
+		if(_k!=null)l.run();
+	};
+	_t.onload=_f;
+	_t.onreadystatechange=function(){
+		if(_t.readyState=="loaded")_f(1);
+	};
+	var _h=document.head||document.getElementsByTagName("head")[0];_h.appendChild(_t);
+}).call(this);
